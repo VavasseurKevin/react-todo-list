47ff96e8be1286c314b8801161ecb6817993f6fe
VavasseurKevin <minhluan.phan@gmail.com>
2023-06-18 15:29:47 +0200

    Defined useState for todo and setTodo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "./components/Layout";
 import Header from "./components/Header";
 import Lists from "./components/Lists";
 import Form from "./components/Form";
 
 const App = () => {
+
+  const [todo, setTodo] = useState('')
 
   const doneHandler = () => {
     console.log('Done')
@@ -17,8 +19,12 @@ const App = () => {
   return (
     <Layout>
       <Header/>
-      <Form/>
-      <Lists done={doneHandler} del={delHandler}/>
+      <Form todo={todo}/>
+      <Lists 
+        done={doneHandler} 
+        del={delHandler} 
+        change={(e) => setTodo(e.target.value)}
+        />
     </Layout>
   
   )
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export const Form = () => {
+export const Form = ({todo, change}) => {
   return (
     <div className='form-group row'>
         <div className='col-8'>
-            <input type='text' className='form-control-plaintext'/>
+            <input type='text' className='form-control-plaintext' value={todo} onChange={change}/>
         </div>
 
         <div className='col-4'>
