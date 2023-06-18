7254cfb9ed708da88fb4eee611f83a454e79c489
VavasseurKevin <minhluan.phan@gmail.com>
2023-06-18 12:54:37 +0200

    Add Lists Components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Layout from "./components/Layout";
 import Header from "./components/Header";
+import Lists from "./components/Lists";
 
 const App = () => {
   return (
     <Layout>
       <Header/>
-      <h1>Bonjour !</h1>
-      Ici, c'est la liste !
+      <Lists/>
     </Layout>
   
   )
---
+++ src/components/Lists.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export const Lists = () => {
+  return (
+    <div className='listWrap'>
+        <ul className='list-group'>
+            <li className='list-group-item d-flex justify-content-between'>
+            List Item
+            <div>
+                <button className='btn btn-sm btn-danger mr-2'>
+                    <i className='far fa-trash-alt'></i>
+                </button>
+                <button className='btn btn-sm btn-success'> 
+                <i className='fas fa-check'></i>
+                </button>
+            </div>
+            </li>
+        
+        </ul>
+    </div>
+  )
+}
+
+export default Lists
