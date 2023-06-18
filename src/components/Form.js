88b2f25c7f444ca5c3b5c3e7791c605125832fde
VavasseurKevin <minhluan.phan@gmail.com>
2023-06-18 21:51:56 +0200

    Fix-Style Text Error

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,20 +4,21 @@ export const Form = ({todo, change, submit, error}) => {
   return (
     <form onSubmit={submit}>
     <div className='form-group row'>
-        <div className='col-8'>
+        <div className='col-9'>
             <input type='text' 
             className='form-control-plaintext' 
             placeholder='Que faire ?'
             value={todo} 
             onChange={change}/>
         </div>
-        {error && <small className='form-text text-danger'>{error}</small>}
-
-        <div className='col-4'>
+        <div className='col-3'>
             <button type='submit' className='btn btn-success'>
-                Ajouter une tâche
+                Ajouter tâche
             </button>
         </div>
+        <div>
+        {error && <small className='form-text text-danger ml-3'>{error}</small>}
+</div>
     </div>
     </form>
   )
