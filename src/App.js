a682987312fb0696c856f2facf9a355b2794c5dd
VavasseurKevin <minhluan.phan@gmail.com>
2023-06-26 20:47:00 +0200

    Add fetchTodos in submitHandler to refresh list todo afer add one + use axios API for delHandler

--- src/App.js
+++ src/App.js
@@ -11,20 +11,32 @@ const App = () => {
   const [todos, setTodos] = useState([]);
   const [error, setError] = useState(null);
 
-   //Get todos List from LocalStorage
-   useEffect(()=>{
+  //Get todos List from LocalStorage
+  useEffect(() => {
     const getTodos = JSON.parse(localStorage.getItem('todos'))
 
-    if(getTodos) {
+    if (getTodos) {
       setTodos(getTodos);
     }
   }, [])
 
+  // Fetch todos from the API
+  const fetchTodos = async () => {
+    try {
+      const response = await axios.get("http://localhost:3000/todos");
+      setTodos(response.data);
+    } catch (error) {
+      console.error("Error fetching todos:", error);
+    }
+  };
+
   // Saving the tasks
-  useEffect(()=>{
+  useEffect(() => {
     localStorage.setItem('todos', JSON.stringify(todos))
   }, [todos])
 
+
+  // Toggling the "done" status of a todo item
   const doneHandler = (todoId) => {
     const index = todos.findIndex(todo => todo.id === todoId);
     const duplicateTodos = [...todos];
@@ -32,27 +44,41 @@ const App = () => {
     duplicateTodos[index] = {
       id: todos[index].id,
       title: todos[index].title,
-      done:!todos[index].done,
+      done: !todos[index].done,
     }
 
     setTodos(duplicateTodos);
     console.log(todos)
   }
 
+
   const delHandler = (todoId) => {
-    if(window.confirm("Êtes-vous sûr ?"))
-    {
-    const updatesTodos = todos.filter(todo => todo.id !== todoId);
-    setTodos(updatesTodos);
-    console.log('Delete');
+    if (window.confirm("Êtes-vous sûr ?")) {
+      // Send the DELETE request to the API
+      axios
+        .delete(`http://localhost:3000/todos/${todoId}`)
+        .then((response) => {
+          if (response.status === 200) {
+            setTodos((prevTodos) => {
+              // Update the todos state by removing the corresponding todo.
+              const updatedTodos = prevTodos.filter((todo) => todo.id !== todoId);
+              console.log("Delete");
+              return updatedTodos;
+            });
+          } else {
+            console.error("La requête de suppression a échoué :", response);
+          }
+        })
+        .catch((error) => {
+          console.error("Erreur lors de la suppression de la tâche :", error);
+        });
     }
-  }
+  };
 
   const submitHandler = (e) => {
     e.preventDefault();
 
-    if (todo.length < 5)
-    {
+    if (todo.length < 5) {
       setError("Une tâche doit contenir au minimum 5 caractères !");
       return false;
     }
@@ -60,10 +86,10 @@ const App = () => {
     axios
       .post("http://localhost:3000/todos", { title: todo })
       .then((response) => {
-        setTodos([{ id: Date.now(), title: todo, done: false }, ...todos]);
-        setError(null);
         setTodo("");
-        console.log(todos);
+        console.log(response.data);
+        // Call fetchTodos to refresh the list after successful addition
+        fetchTodos();
       })
       .catch((error) => {
         console.error("Erreur lors de la requête :", error);
@@ -72,20 +98,20 @@ const App = () => {
 
   return (
     <Layout>
-      <Header/>
-      <Form 
-        todo={todo} 
-        submit={submitHandler} 
+      <Header />
+      <Form
+        todo={todo}
+        submit={submitHandler}
         change={(e) => setTodo(e.target.value)}
         error={error}
-        />
-      <Lists 
-        done={doneHandler} 
-        del={delHandler} 
+      />
+      <Lists
+        done={doneHandler}
+        del={delHandler}
         todos={todos}
-        />
+      />
     </Layout>
-  
+
   )
 }
 
